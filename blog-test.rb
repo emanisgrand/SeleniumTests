1c94ab7f94db29e90bb32ec170742ce24d848882
Emanonymous <emanisgrand@users.noreply.github.com>
2018-10-26 19:47:01 -0400

    Add files via upload

--- blog-test.rb
+++ blog-test.rb
@@ -1,34 +1,32 @@
 require "selenium-webdriver"
 require "rspec"
+require_relative "signup_page.rb"
+require_relative "users_page.rb"
+
+timestamp = Time.now.to_i
+username = "user#{timestamp}"
+email = "user#{timestamp}@test.com"
+password = "password"
+expected_banner_text = "Welcome to the alpha blog user#{timestamp}"
 
 # TEST: Sign up for blog
 describe "Blog application" do
-	describe "signup to the blog application" do
-		it "confirm that the user can successfully signup" do
-			timestamp = Time.now.to_i
-			driver = Selenium::WebDriver.for :firefox
-			# Go to signup form
-			driver.navigate.to "https://selenium-blog.herokuapp.com/signup"
-
-			#Fill out and submit form
-			username_field = driver.find_element(id: "user_username")
-			username_field.send_keys("Primpto #{timestamp}")
-
-			email_field = driver.find_element(id: 'user_email')
-			email_field.send_keys("user#{timestamp}@test.com")
-
-			password_field = driver.find_element(id: "user_password")
-			password_field.send_keys("disaniggaspw")
-
-			sign_up_button = driver.find_element(id: "submit")
-			sign_up_button.click
-
-			# Confirm user is signed up successfully
-			banner = driver.find_element(id: "flash_success")
-			banner_text = banner.text 
-			expect(banner_text).to eq("Welcome to the alpha blog user#{timestamp}")
-
-			driver.quit
-		end
-	end
+  describe "when signing up a new user"	do
+  	it "creates a new user account successfully" do
+		  @driver = Selenium::WebDriver.for :firefox
+      # Go to signup form
+      @driver.navigate.to "https://selenium-blog.herokuapp.com/signup"
+		  # Fill out and submit form
+      signup = SignupPage.new(@driver)
+      signup.enter_username(username)
+      signup.enter_email_address(email)
+      signup.enter_password(password)
+      signup.submit_form()
+		  # Confirm user is signed up successfully
+      users = UsersPage.new(@driver)
+      banner_text = users.get_banner_text()
+      expect(banner_text).to eq(expected_banner_text)
+		  @driver.quit
+	  end
+  end
 end
